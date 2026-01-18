d909b9df5e2322e9b1d139a383b18d97ca9a51ad
ANANYA160805 <ananyasinha1599@gmail.com>
2026-01-18 16:37:05 +0530

    first commit

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year = sc.nextInt();
+
+        if (year%400==0||(year%4==0 && year%100!=0)){
+            System.out.println("Leap year:"+year);
+        }else {
+            System.out.println("not a leap year:"+year);
+        }
+
+    }
 }
