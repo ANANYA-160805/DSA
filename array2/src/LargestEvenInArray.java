2470954504bc765d8a816b3fd847515aff8fa943
ANANYA160805 <ananyasinha1599@gmail.com>
2026-01-23 09:12:15 +0530

    first commit

--- array2/src/LargestEvenInArray.java
+++ array2/src/LargestEvenInArray.java
@@ -1,6 +1,6 @@
 //Finding the Largest Even Number in an Array If no even number exists, return -1.//
 public class LargestEvenInArray {
-    public static int LargestEven(int[] arr){
+    public static int largestEven(int[] arr){
         int maxEven=-1;
         for(int i = 0; i < arr.length ; i++) {
             if(arr[i]%2==0){
@@ -12,7 +12,7 @@ public class LargestEvenInArray {
     public static void main(String[] args) {
         int[] arr={ 2,4,-1,6,8};
         int[] arr2={4,7,8,12,50};
-        System.out.println(LargestEven(arr));
-        System.out.println(LargestEven(arr2));
+        System.out.println(largestEven(arr));
+        System.out.println(largestEven(arr2));
     }
 }
---
+++ array2/src/ReverseOfArray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ReverseOfArray {
+    public static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, -2, 45, 7, 3};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
