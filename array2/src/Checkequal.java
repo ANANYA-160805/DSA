838ebebb5a93d77411257dabb0ce5bbffe0c2d9d
ANANYA160805 <ananyasinha1599@gmail.com>
2026-01-30 21:19:59 +0530

    first commit

--- array2/src/Checkequal.java
+++ array2/src/Checkequal.java
@@ -1,4 +1,24 @@
 package PACKAGE_NAME;
 
 public class Checkequal {
+    static void main(String[] args) {
+        int[]  a = {1,2,3,4,5,6,7,8,9,10};
+        int[]  b = {1,2,3,4,5,6,7,8,10};
+
+        System.out.println(equal(a,b));
+    }
+    public static boolean equal(int[] a,int[] b){
+        if (a.length != b.length) {
+            return false;
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
