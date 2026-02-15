07ffe912bd71797250e4399031eb009ec5cf7324
ANANYA160805 <ananyasinha1599@outlook.com>
2026-02-15 15:33:03 +0530

    first commit

--- Basic_Sorting/src/BubbleSort.java
+++ Basic_Sorting/src/BubbleSort.java
@@ -25,8 +25,9 @@ public class BubbleSort {
 
 //        worst case time complexity=> o(n^2)
 
-        int swap=0;
+
         for (int i = 0; i <n-1 ; i++) {
+            int swap=0;
             for (int j = 0; j <n-1-i ; j++) {
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
