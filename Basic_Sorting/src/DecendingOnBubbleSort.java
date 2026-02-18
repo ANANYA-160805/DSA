15aa3143f5a2ac304f3b23fa6a17d9336ae68987
ANANYA160805 <ananyasinha1599@outlook.com>
2026-02-19 00:28:39 +0530

    first commit

---
+++ Basic_Sorting/src/DecendingOnBubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class DecendingOnBubbleSort {
+    public static void main(String[] args) {
+        int[] arr={9 ,10, 11 ,1 ,26, 38};
+        int n=arr.length;
+
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        printArray(arr, n);
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
+    public static void printArray(int[] arr, int n){
+
+        for (int i = 0; i <n-1 ; i++) {
+
+            boolean isSorted=true;
+            for (int j = 0; j <n-1-i ; j++) {
+                if(arr[j]<arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    isSorted=false;
+                }
+            }
+            if(isSorted==true)break;
+
+        }
+    }
+}
