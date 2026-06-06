61d3ef96dc3dbf98a587653f550647016219d0ec
ANANYA160805 <ananyasinha1599@outlook.com>
2026-06-06 22:04:49 +0530

    linked list basics

--- LinkedList/src/MiddleOfLinkedList.java
+++ LinkedList/src/MiddleOfLinkedList.java
@@ -15,17 +15,28 @@ public class MiddleOfLinkedList {
         }
     }
     public Node listnode(Node head){
-        int length = 0;
-        Node temp = head;
+//        int length = 0;
+//        Node temp = head;
+//
+//        while(temp!=null){
+//            temp = temp.next;
+//            length++;
+//        }
+//        temp = head;
+//        for(int i=1; i<=length/2; i++){
+//            temp = temp.next;
+//        }
+//        return temp;
 
-        while(temp!=null){
-            temp = temp.next;
-            length++;
+        //slow-fast approach
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        temp = head;
-        for(int i=1; i<=length/2; i++){
-            temp = temp.next;
-        }
-        return temp;
+        return slow;
     }
 }
